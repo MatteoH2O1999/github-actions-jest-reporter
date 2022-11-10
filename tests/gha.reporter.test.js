7bf6c625cf382c9baae0117f6fd7602bd3c3b4db
Matteo Dell'Acqua <82184604+MatteoH2O1999@users.noreply.github.com>
2022-11-10 11:41:31 +0100

    Begin unit testing scaffolding

--- tests/gha.reporter.test.js
+++ tests/gha.reporter.test.js
@@ -1,4 +1,22 @@
 import {describe, test, expect} from '@jest/globals';
+import GhaReporter from '../src'
 
-test('dummy', () => {
+test('can be instantiated', () => {
+    const gha = new GhaReporter();
+    expect(gha).toBeTruthy();
+    expect(gha).toBeInstanceOf(GhaReporter);
+})
+
+describe('Result tree generation', () => {
+})
+
+describe('Result tree output', () => {
+})
+
+describe('Reported interface', () => {
+    test('onRunComplete', () => {
+    })
+
+    test('onTestResult', () => {
+    })
 })
